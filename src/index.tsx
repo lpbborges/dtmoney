a4520af1ec7a208e1eb7792bca0877429a023d6a
LP <luizpaulobborges@gmail.com>
2021-06-24 16:04:49 -0300

    added modal and form

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          amount: 6000,
+          type: 'deposit',
+          category: 'Dev',
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          amount: 600,
+          type: 'withdraw',
+          category: 'Casa',
+          createdAt: new Date('2021-05-20 12:00:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      
+      return schema.create('transaction', data);
+    });
   }
 })
 
